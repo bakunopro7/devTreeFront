9647358e55491c7b99f8f78b5deb24f103239e5f
jesus <jabakabaka@gmail.com>
2025-01-21 19:53:43 -0600

    Obtencion de componente y vizualisación de la data

--- src/api/DevTreeAPI.ts
+++ src/api/DevTreeAPI.ts
@@ -1,9 +1,10 @@
 import { isAxiosError } from "axios";
 import api from "../config/axios";
+import { User } from "../types";
 
 export async function getUser() {
     try {
-        const { data } = await api('/user')
+        const { data } = await api<User>('/user')
         console.log(data)
         return data
     } catch (error) {
