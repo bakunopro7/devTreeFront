e662539cf803c62789ea9fedd90de92c3041931e
jesus <jabakabaka@gmail.com>
2025-02-27 23:54:30 -0600

    se agrega tipeo a social network

--- src/views/LinkTreeView.tsx
+++ src/views/LinkTreeView.tsx
@@ -1,5 +1,11 @@
+import { useState } from "react";
+import { social } from "../data/social";
+
 
 function LinkTreeView() {
+    const [socialLinks, setSocialLinks] = useState(social);
+
+
     return (
         <div></div>
     );
